05e2c0dfa32bf84de190b0431333ea0054585a65
LenovoPC <1162794333@qq.com>
2020-04-20 19:27:22 +0800

    initial

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+import os
+
+# yolo
+TRAIN_INPUT_SIZE = [320 + i * 32 for i in range(10)]
+
+# train
+BATCH_SIZE = 1024
+EPOCHS = 300
+
+# test
+TEST_INPUT_SIZE = 544
+
+# name and path
+PROJECT_PATH = os.getcwd()
+DATA_PATH = os.path.join(os.getcwd(), 'data')
 CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
            'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
            'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
--- data/data.py
+++ data/data.py
@@ -3,4 +3,12 @@ import config as cfg
 import os
 
 
+class Data:
+    def __init__(self):
+        pass
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        pass
