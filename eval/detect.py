1ad55f8957757df51de30e55ffc1efaee9aa0ed0
LenovoPC <1162794333@qq.com>
2020-04-24 12:42:19 +0800

    basedd

--- eval/detect.py
+++ eval/detect.py
@@ -1,30 +1,33 @@
 import numpy as np
 import tensorflow as tf
+from PIL import Image
+
 import config as cfg
-from data.utils import resize_to_train_size
+from data.utils import resize_to_train_size, draw_image_with_boxes
 from eval.utils import iou_calc1, nms
 from model.net import get_yolo_model
 from model.utils import decode
 
 
 class YoloDetect:
-    def __init__(self):
-        self.model = get_yolo_model()
-        checkpoint = tf.train.Checkpoint(m=self.model)
-        checkpoint.restore(tf.train.latest_checkpoint(cfg.CHECKPOINT_PATH))
-        pass
+    def __init__(self, model=None):
+        if model == None:
+            self.model = get_yolo_model()
+            checkpoint = tf.train.Checkpoint(m=self.model)
+            checkpoint.restore(tf.train.latest_checkpoint(cfg.CHECKPOINT_PATH))
+        else:
+            self.model = model
 
-    def get_boxes(self, images):
+    def detect_image(self, annotation_line):
         '''
         :param data:[1,cfg.TEST_INPUT_SIZE,cfg.TEST_INPUT_SIZE,3]
         :return:
         '''
-        images = np.array(images, dtype=np.float32)
-        input_data = []
-        for image in images:
-            input_data.append(resize_to_train_size(image.copy(), cfg.TEST_INPUT_SIZE, is_training=False))
-        input_data = np.concatenate(input_data, axis=0)
-        pred_sbbox, pred_mbbox, pred_lbbox = self.model(input_data)
+        image = Image.open(annotation_line.split()[0])
+        image = np.array(image)
+        resized_image = resize_to_train_size(image.copy(), cfg.TEST_INPUT_SIZE, is_training=False) / 255.0
+        pred_sbbox, pred_mbbox, pred_lbbox = self.model(
+            resized_image(tf.convert_to_tensor(resized_image[np.newaxis, :, :, :]).astype(np.float32)))
         pred_sbbox = tf.reshape(decode(pred_sbbox, 8), shape=[-1, 25])
         pred_mbbox = tf.reshape(decode(pred_mbbox, 16), shape=[-1, 25])
         pred_lbbox = tf.reshape(decode(pred_lbbox, 32), shape=[-1, 25])
@@ -34,8 +37,8 @@ class YoloDetect:
         pred_msk = tf.reduce_max(pred_boxes_info[..., 4:5] * pred_boxes_info[..., 5:], axis=-1) > cfg.IGNORE_THRESH
         pred_boxes_coor = tf.boolean_mask(pred_boxes_info[..., :4], tf.cast(pred_msk, dtype=tf.bool)).numpy()
 
-        pred_msk = np.logical_or(pred_boxes_coor[:, 0] > pred_boxes_coor[:, 2],
-                                 pred_boxes_coor[:, 1] > pred_boxes_coor[:, 3])
+        pred_msk = np.logical_or(pred_boxes_coor[:, 0] >= pred_boxes_coor[:, 2],
+                                 pred_boxes_coor[:, 1] >= pred_boxes_coor[:, 3])
         pred_class_prob = pred_class_prob[pred_msk]
         pred_boxes_coor = pred_boxes_coor[pred_msk]
         pred_boxes_coor[:, 0:2] = np.maximum([0.0, 0.0], pred_boxes_coor[:, 0:2])
@@ -43,6 +46,6 @@ class YoloDetect:
                                              pred_boxes_coor[:, 2:4])
 
         clazz = np.argmax(pred_class_prob, axis=-1)
-        prob = pred_class_prob[clazz]
+        prob = pred_class_prob[np.arange(len(pred_class_prob)), clazz]
         pred = np.concatenate([pred_boxes_coor, prob[:, np.newaxis], clazz[:, np.newaxis]], axis=-1)
-        return image, nms(pred)
+        draw_image_with_boxes(image, nms(pred), 'test.png')
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import numpy as np
 import config as cfg
 import tensorflow as tf
 from data.data import Data
+from eval.detect import YoloDetect
 from model.loss import yolo_loss
 from model.net import get_yolo_model
 from model.utils import get_lr
@@ -18,7 +19,7 @@ test_loss = []
 start = tf.Variable(initial_value=0)
 check_point = Checkpoint(m=model, optim=optimizer, s=start)
 manager = CheckpointManager(check_point, cfg.CHECKPOINT_PATH, 3)
-
+detect = YoloDetect(model=model)
 if (cfg.RESTORE_TRAINING and tf.train.latest_checkpoint(cfg.CHECKPOINT_PATH)):
     check_point.restore(tf.train.latest_checkpoint(cfg.CHECKPOINT_PATH))
 
@@ -32,7 +33,7 @@ for i in tf.range(start.numpy(), cfg.EPOCHS):
             loss_val = yolo_loss(pred_sbbox, pred_mbbox, pred_lbbox, label_sbbox, label_mbbox, label_lbbox)
         grads = tape.gradient(loss_val, model.trainable_variables)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
-        tf.summary.scalar('train_loss', loss_val, optimizer.iterations)
+        # tf.summary.scalar('train_loss', loss_val, optimizer.iterations)
         if (optimizer.iterations % 50 == 0):
             tf.print(optimizer.iterations, 'train_loss', loss_val, 'lr:', lr)
     for image, label_sbbox, label_mbbox, label_lbbox in test_data:
@@ -40,7 +41,8 @@ for i in tf.range(start.numpy(), cfg.EPOCHS):
         loss_val = yolo_loss(pred_sbbox, pred_mbbox, pred_lbbox, label_sbbox, label_mbbox, label_lbbox)
         test_loss.append(loss_val.numpy())
     print("test loss:", np.mean(test_loss))
-    tf.summary.scalar('test_loss', loss_val / test_data.get_size(), optimizer.iterations)
-    tf.summary.scalar('lr', optimizer.lr, step=optimizer.iterations)
-    test_loss.assign(tf.constant(0, dtype=tf.float32))
+    detect.detect_image(r'/content/yolov3_with_tricks/data/VOC/2007_trainval/JPEGImages/000007.jpg 141,50,500,330,6')
+    # tf.summary.scalar('test_loss', loss_val / test_data.get_size(), optimizer.iterations)
+    # tf.summary.scalar('lr', optimizer.lr, step=optimizer.iterations)
+    # test_loss.assign(tf.constant(0, dtype=tf.float32))
     manager.save()
